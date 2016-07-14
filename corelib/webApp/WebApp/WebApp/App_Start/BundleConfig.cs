2820103ae66f3116a8a683bfef59b368414dff9d
zolids <jenno.cabrito@gmail.com>
2016-07-14 18:02:47 +0400

    add loading screen

--- corelib/webApp/WebApp/WebApp/App_Start/BundleConfig.cs
+++ corelib/webApp/WebApp/WebApp/App_Start/BundleConfig.cs
@@ -23,7 +23,9 @@ namespace WebApp
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css",
+                        "~/Content/app.css",
+                        "~/Content/loading-screen.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
