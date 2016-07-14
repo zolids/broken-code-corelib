60e2acf91d4cf37c4f98ec6cdb5f53630ad88225
zolids <jenno.cabrito@gmail.com>
2016-07-14 14:12:07 +0400

    update repo

---
+++ corelib/webApp/WebApp/WebApp/DbContext/dbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class dbContext : DbContext
+    {
+        public dbContext() : base("dbcontext_fsxcv"){
+
+            Database.SetInitializer<dbContext>(null);
+
+        }
+    }
+}
---
+++ corelib/webApp/WebApp/WebApp/DbModels/users.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class users
+    {
+        [Table("gsa_users")]
+        public class Users
+        {
+            [Key]
+            [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+            [Display(Name = "User ID")]
+            public long user_id { get; set; }
+
+            [Required]
+            [Display(Name = "Username")]
+            public string username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string password { get; set; }
+
+            [NotMapped]
+            [Display(Name = "Remember Me")]
+            public bool RememberMe { get; set; }
+
+            [Display(Name = "Full Name")]
+            public string name { get; set; }
+
+            [Display(Name = "Position")]
+            public string position { get; set; }
+
+            public int usertype { get; set; }
+
+            [Display(Name = "User Type")]
+            public string usertypedisp { get; set; }
+
+            public int rfqApprover { get; set; }
+
+            [Display(Name = "RFQ Approver")]
+            public string rfqApproveryn { get; set; }
+
+            public int rfqAmount { get; set; }
+
+            [Display(Name = "E-Mail")]
+            public string email { get; set; }
+
+            [Display(Name = "Status")]
+            public string Status { get; set; }
+
+            [NotMapped]
+            public bool rfqApproverBool
+            {
+                get
+                {
+                    return rfqApprover == 0 ? false : true;
+                }
+                set
+                {
+                    rfqApprover = value ? 1 : 0;
+                }
+            }
+
+        }
+
+        [Table("gsa_user_sites")]
+        public class UserSites
+        {
+            [Key]
+            [Column(Order = 1)]
+            public long user_id { get; set; }
+
+            [Key]
+            [Column(Order = 0)]
+            public long gus_site_id { get; set; }
+        }
+    }
+}
